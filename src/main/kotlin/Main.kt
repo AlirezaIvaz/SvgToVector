24043e42fd60d766a735b54f70c78a15f4526b9a
AlirezaIvaz <ivaz1382@gmail.com>
2024-05-16 20:32:22 +0330

    Improve CLI functionality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,47 @@
 fun main(args: Array<String>) {
-    println("Welcome to SvgToVector convertor CLI...")
+    println("SVG to XML resource convertor CLI")
     println("----")
-    if (args.isEmpty()) {
-        println("Error: Source directory not specified!")
-    } else {
-        val sourceDirectory = args[0]
-        val processor = SvgFilesProcessor(sourceDirectory)
-        processor.process()
+    val defaultSource: String = System.getProperty("user.dir")
+    val defaultDestination = "$defaultSource/output"
+
+    print("Source directory (Default: $defaultSource): ")
+    val sourceDir = readlnOrNull() ?: defaultSource
+
+    print("Destination directory (Default: $defaultDestination): ")
+    val destinationDir = readlnOrNull() ?: defaultDestination
+
+    print("Do you want to replace any text in output files? [Y/n] ")
+    val needReplace = readlnOrNull().let {
+        (it ?: "n").lowercase() == "y"
     }
+    val replacement = getReplacements(needReplace)
+
+    val processor = SvgFilesProcessor(
+        sourceDir,
+        destinationDir,
+        replacement
+    )
+    processor.process()
+}
+
+fun getReplacements(needReplace: Boolean): Map<String, String> {
+    if (!needReplace) return emptyMap()
+
+    val replacements = mutableMapOf<String, String>()
+
+    while (true) {
+        print("Enter the text to replace (or press Enter to finish): ")
+        val textToReplace = readlnOrNull()
+
+        if (textToReplace.isNullOrBlank()) {
+            break
+        }
+
+        print("Enter the replacement text: ")
+        val replacementText = readlnOrNull() ?: ""
+
+        replacements[textToReplace] = replacementText
+    }
+
+    return replacements
 }
